8075c068d8286ffbe12c729e06736add9f6e41a5
L_GS <jscyclops@gmail.com>
2024-03-29 12:36:36 -0400

    drop in initial testing for oauth flow

---
+++ src/main/java/com/lordgiacomos/cachedauth/api/Authenticator.java
@@ -0,0 +1,54 @@
+package com.lordgiacomos.cachedauth.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class Authenticator {
+    public static final String URL = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+    public static final String CLIENT_ID = System.getenv("CLIENT_ID");
+    public static final String SCOPES = "user.read"; // need to add `XboxLive.signin` scope here... I think
+
+    public static URI setupURI() throws URISyntaxException{
+        return new URIBuilder(URL)
+                .addParameter("client_id", CLIENT_ID)
+                .addParameter("scope", SCOPES)
+                .build();
+    }
+
+    public static String getResponse(URI uri, CloseableHttpClient client) throws IOException {
+        String result = "";
+        HttpGet request = new HttpGet(uri);
+        try (CloseableHttpResponse response = client.execute(request)) {
+            System.out.println(response.getStatusLine().getStatusCode());
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                result = EntityUtils.toString(entity);
+            }
+        }
+        return result;
+    }
+
+    public static void test() {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            URI uri = setupURI();
+            String result = getResponse(uri, client);
+            MSAResponse parsed = new MSAResponse(result);
+        } catch (URISyntaxException ex) {
+            System.out.println("uri syntax problems");
+            System.out.println(ex.getMessage());
+        } catch (IOException ex2) {
+            System.out.println("io problems");
+            System.out.println(ex2.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/lordgiacomos/cachedauth/api/MSAResponse.java
@@ -0,0 +1,26 @@
+package com.lordgiacomos.cachedauth.api;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+class MSAResponse { //should be able to slim this code down and bury it in another class once I'm done with development
+    String userCode;
+    String deviceCode;
+    URI verificationUri;
+    int expiresInSeconds;
+    int interval; // how often (in seconds), client should check if code has been submitted through microsoft account
+    String message;
+
+    MSAResponse(String responseString) throws URISyntaxException {
+        JsonObject json = JsonParser.parseString(responseString).getAsJsonObject();
+        userCode = json.get("user_code").getAsString();
+        deviceCode = json.get("device_code").getAsString();
+        verificationUri = new URI(json.get("verification_uri").getAsString());
+        expiresInSeconds = json.get("expires_in").getAsInt();
+        interval = json.get("interval").getAsInt();
+        message = json.get("message").getAsString();
+    }
+}
